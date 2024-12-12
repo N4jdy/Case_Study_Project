4e61e72c2218323ebabcc11fbac8d40d9decc33f
Jan Naidr <nj0562@mci4me.at>
2024-12-12 12:13:56 +0100

    requirements push

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import streamlit as st
-st.write("## Wartungskosten")
 
-# Callbacks for the number inputs
-def update_price_from_euro():
-    st.session_state.price_dollar = st.session_state.price_euro * 1.1
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
-def update_price_from_dollar():
-    st.session_state.price_euro = st.session_state.price_dollar * 0.9
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
-# Number inputs for the maintenance costs
-st.number_input(label="Wartungskosten in Euro", 
-                            key = "price_euro",
-                            on_change=update_price_from_euro)
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
-st.number_input(label="Wartungskosten in Dollar", 
-                            key = "price_dollar",
-                            on_change=update_price_from_dollar)
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
